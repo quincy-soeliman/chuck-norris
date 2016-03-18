8c881b07160fe25d8a34f03de6de6862d222c28d
quincy.soeliman <quincy.soeliman@indicia.nl>
2016-03-18 09:25:20 +0100

    Pushed another one

---
+++ js/app.js
@@ -0,0 +1,24 @@
+angular
+  .module('chuckApp', [])
+  .factory('chuckFactory', ['$http', function($http){
+    var urlBase = 'http://api.icndb.com/jokes';
+
+    var chuckFactory = {};
+
+    chuckFactory.getRandomJoke = function(firstName, lastName) {
+      return $http.get(urlBase + '/random?firstName=' + firstName + '&lastName=' + lastName);
+    };
+
+    return chuckFactory;
+  }])
+  .controller('chuckController', ['$scope', 'chuckFactory', function($scope, chuckFactory) {
+    $scope.person = {};
+    $scope.joke = '';
+
+    $scope.getRandomJoke = function(firstName, lastName) {
+      chuckFactory.getRandomJoke(firstName, lastName).then(function(response) {
+        var joke = response.data.value.joke;
+        $scope.joke = joke.replace(/&quot;/ig, '"');
+      });
+    };
+  }]);
